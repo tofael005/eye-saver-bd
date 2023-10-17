1da820feec9be3193d0b24568b815df5a664ff48
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-17 16:05:52 +0600

    root file setup

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,18 @@
 import Home from "../Home/Home";
+import Root from "../Layout/Root";
 
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />
+        element: <Root />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
     }
 ])
 export default router;
