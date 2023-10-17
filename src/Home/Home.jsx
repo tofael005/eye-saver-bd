45d893711899ce04808bd3f58e32014f0df65013
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-17 17:03:46 +0600

    banner added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Navbar/Banner";
+import About from "../Page/About";
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <About />
         </div>
     );
 };
--- src/Navbar/NavBar.jsx
+++ src/Navbar/NavBar.jsx
@@ -3,13 +3,13 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const NavBar = () => {
     return (
-        <div className="flex justify-between items-center bg-slate-300">
+        <div className="flex justify-between items-center bg-sky-400 px-3">
             <div>
-                <img className="w-[75px] " src="/src/assets/png.png" alt="" />
+                <img className="w-[75px] cursor-pointer" src="/src/assets/png.png" alt="" />
             </div>
             <div className="flex justify-between items-center gap-7">
-                <CgProfile />
-                <AiOutlineShoppingCart />
+                <AiOutlineShoppingCart className='text-3xl cursor-pointer' />
+                <CgProfile className='text-3xl cursor-pointer' />
             </div>
         </div>
     );
